95763d3c2f69f67e5fd83fd163a5dd7a70af605f
Martin <mgarcas320@g.educaand.es>
2025-01-17 09:55:48 +0100

    new springboot project

---
+++ primera/src/main/java/com/jve/primera/controllers/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.jve.primera.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+	@GetMapping("/")
+    public String holaMundo() {
+        return "hola-mundo";
+    }
+}
+
---
+++ primera/src/test/java/com/jve/primera/PrimeraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jve.primera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
